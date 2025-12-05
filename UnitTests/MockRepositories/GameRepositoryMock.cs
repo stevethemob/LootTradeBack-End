06293a2d7d39bc3b1363032aa4f53ababcb7dc0e
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2025-12-05 20:14:49 +0100

    added first test so i can set up sonarqube and see code coverage that way

---
+++ UnitTests/MockRepositories/GameRepositoryMock.cs
@@ -0,0 +1,25 @@
+﻿using LootTradeDTOs;
+using LootTradeInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.MockRepositories
+{
+    public class GameRepositoryMock : IGameRepository
+    {
+        public List<GameDTO> GetAllGames()
+        {
+            List<GameDTO> gameDTOs = new List<GameDTO>();
+
+            GameDTO gameDTO = new GameDTO();
+            gameDTO.Id = 1;
+            gameDTO.Title = "test";
+            gameDTOs.Add(gameDTO);
+
+            return gameDTOs;
+        }
+    }
+}
---
+++ UnitTests/Tests/GameTest.cs
@@ -0,0 +1,22 @@
+﻿using LootTradeDomainModels;
+using LootTradeServices;
+using UnitTests.MockRepositories;
+
+namespace UnitTests.Tests
+{
+    [TestClass]
+    public sealed class GameTest
+    {
+        [TestMethod]
+        public void GetAllGamesTest1Game()
+        {
+            GameRepositoryMock gameRepositoryMock = new GameRepositoryMock();
+            GameService gameService = new GameService(gameRepositoryMock);
+            List<Game> games = new List<Game>();
+
+            games = gameService.GetAllGames();
+
+            Assert.IsTrue(games.Count == 1);
+        }
+    }
+}
