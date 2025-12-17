52576033df3ccdc8b7bbb73dd5b7c79e4531b47e
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2025-12-17 21:01:42 +0100

    added tests for the new feature in offer service

---
+++ UnitTests/MockRepositories/OfferRepositoryMock.cs
@@ -0,0 +1,39 @@
+﻿using LootTradeInterfaces;
+using LootTradeDTOs;
+
+namespace UnitTests.MockRepositories
+{
+    public class OfferRepositoryMock : IOfferRepository
+    {
+        public bool AddOffer(int inventoryId)
+        {
+            return true;
+        }
+
+        public List<OfferDTO> GetAllOffersByGameId(int gameId)
+        {
+            List<OfferDTO> offerDTOs = new List<OfferDTO>();
+
+            OfferDTO offer1 = new OfferDTO();
+            offer1.Id = 1;
+            offer1.DateTimeOpen = DateTime.Now;
+            offer1.Item.Id = 1;
+            offer1.Item.GameId = gameId;
+            offer1.Item.Description = "test";
+            offer1.Item.Name = "test";
+            
+            OfferDTO offer2 = new OfferDTO();
+            offer2.Id = 2;
+            offer2.DateTimeOpen = DateTime.Now;
+            offer2.Item.Id = 2;
+            offer2.Item.GameId = gameId;
+            offer2.Item.Description = "test";
+            offer2.Item.Name = "test";
+
+            offerDTOs.Add(offer1);
+            offerDTOs.Add(offer2);
+
+            return offerDTOs;
+        }
+    }
+}
---
+++ UnitTests/Tests/OfferTest.cs
@@ -0,0 +1,40 @@
+﻿using LootTradeDomainModels;
+using LootTradeServices;
+using UnitTests.MockRepositories;
+
+namespace UnitTests.Tests
+{
+    [TestClass]
+    public class OfferTest
+    {
+        [TestMethod]
+        public void AddOfferTestTrue()
+        {
+            OfferRepositoryMock offerRepository = new OfferRepositoryMock();
+            InventoryRepositoryMock inventoryRepository = new InventoryRepositoryMock();
+
+            OfferService offerService = new OfferService(offerRepository, inventoryRepository);
+            int userId = 1;
+            int itemId = 1;
+
+            bool success = offerService.AddOffer(userId, itemId);
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void GetAllOffersByGameIdCount2()
+        {
+            OfferRepositoryMock offerRepository = new OfferRepositoryMock();
+            InventoryRepositoryMock inventoryRepository = new InventoryRepositoryMock();
+
+            OfferService offerService = new OfferService(offerRepository, inventoryRepository);
+
+            int gameId = 1;
+
+            List<Offer> offers = offerService.GetAllOffersByGameId(gameId);
+
+            Assert.AreEqual(offers.Count, 2);
+        }
+    }
+}
