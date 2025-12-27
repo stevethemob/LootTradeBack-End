485d05038aa3dec0d095ef55c7a29d3edcc2c1a2
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2025-12-27 17:06:19 +0100

    fixed loginrequest for medium severity sonarqube issue

--- DomainModels/LoginRequest.cs
+++ DomainModels/LoginRequest.cs
@@ -8,6 +8,11 @@ namespace LootTradeDomainModels
 {
     public class LoginRequest
     {
+        public LoginRequest(string username, string password) 
+        {
+            Username = username;
+            Password = password;
+        }
         public string Username { get; set; }
         public string Password { get; set; }
     }
