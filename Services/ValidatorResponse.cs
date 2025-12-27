bc53b5ab8e9fb7e2f2671c44e0ababd676926544
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2025-12-27 23:12:36 +0100

    sonarqube validatorresponse

--- Services/ValidatorResponse.cs
+++ Services/ValidatorResponse.cs
@@ -5,6 +5,6 @@ namespace LootTradeServices
     public class ValidatorResponse
     {
         public bool Success { get; set; }
-        public List<string> Errors { get; set; }
+        public List<string>? Errors { get; set; }
     }
 }
