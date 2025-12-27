5bca1c25fba70b868169431abb3b186f5f5f589a
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2025-12-27 22:43:21 +0100

    fixed sonarqube warnings in jwtService

--- LootTradeApiCS/JwtService.cs
+++ LootTradeApiCS/JwtService.cs
@@ -24,7 +24,7 @@ namespace LootTradeApiCSJwtService
         };
 
             var key = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(_config["Jwt:Key"])
+                Encoding.UTF8.GetBytes(_config["Jwt:Key"]!)
             );
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -34,7 +34,7 @@ namespace LootTradeApiCSJwtService
                 audience: _config["Jwt:Audience"],
                 claims: claims,
                 expires: DateTime.UtcNow.AddMinutes(
-                    double.Parse(_config["Jwt:ExpiresInMinutes"])
+                    double.Parse(_config["Jwt:ExpiresInMinutes"]!)
                 ),
                 signingCredentials: creds
             );
