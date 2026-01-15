7da62ec3637716ef50e2c2029edb3c2ee4f9bf15
Laurensdegroot1 <150124584+Laurensdegroot1@users.noreply.github.com>
2026-01-15 23:47:16 +0100

    added tradetest

---
+++ UnitTests/MockRepositories/TradeRepositoryMock.cs
@@ -0,0 +1,54 @@
+﻿using LootTradeDTOs;
+using LootTradeInterfaces;
+
+namespace UnitTests.MockRepositories
+{
+    public class TradeRepositoryMock : ITradeRepository
+    {
+        public bool AddTradeOffer(int offerId, List<int> itemIds, int traderId)
+        {
+            return true;
+        }
+
+        public AllTradesDTO GetAllTradeIdsByGameIdAndUserId(int gameId, int userId)
+        {
+            AllTradesDTO trades = new AllTradesDTO();
+
+            trades.TradeIds.Add(1);
+            trades.TraderUsernames.Add("test");
+            trades.TradeIds.Add(2);
+            trades.TraderUsernames.Add("test2");
+
+            return trades;
+        }
+
+        public TradeDTO GetTradeByTradeId(int tradeId)
+        {
+            ItemDTO item = new ItemDTO(1, 1, "test", "test");
+
+            TradeDTO trade = new TradeDTO(1, item, "test");
+
+            return trade;
+        }
+
+        public bool AcceptTrade(int tradeId)
+        {
+            return true;
+        }
+
+        public bool CheckIfTradeIsBySameUser(int tradeId, int userId)
+        {
+            return false;
+        }
+
+        public List<TradeAdminDTO> GetAllTradesByGameId(int gameId)
+        {
+            List<TradeAdminDTO> trades = new List<TradeAdminDTO>();
+
+            TradeAdminDTO trade = new TradeAdminDTO(1, "test", "test");
+            trades.Add(trade);
+
+            return trades;
+        }
+    }
+}
---
+++ UnitTests/Tests/TradeTest.cs
@@ -0,0 +1,95 @@
+﻿using LootTradeDomainModels;
+using LootTradeServices;
+using UnitTests.MockRepositories;
+
+namespace UnitTests.Tests
+{
+    [TestClass]
+    public class TradeTest
+    {
+        [TestMethod]
+        public void AddTradeOfferTestTrue()
+        {
+            TradeRepositoryMock tradeRepositoryMock = new TradeRepositoryMock();
+            TradeService tradeService = new TradeService(tradeRepositoryMock);
+
+            int offerId = 1;
+            List<int> itemIds = new List<int>();
+            int traderId = 1;
+            itemIds.Add(1);
+
+            bool success = tradeService.AddTradeOffer(offerId, itemIds, traderId);
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void AddTradeOfferTestFalse()
+        {
+            TradeRepositoryMock tradeRepositoryMock = new TradeRepositoryMock();
+            TradeService tradeService = new TradeService(tradeRepositoryMock);
+
+            int offerId = 1;
+            List<int> itemIds = new List<int>();
+            int traderId = 1;
+
+            bool success = tradeService.AddTradeOffer(offerId, itemIds, traderId);
+
+            Assert.IsFalse(success);
+        }
+
+        [TestMethod]
+        public void GetAllTradeIdsByGameIdAndUserIdTest()
+        {
+            TradeRepositoryMock tradeRepositoryMock = new TradeRepositoryMock();
+            TradeService tradeService = new TradeService(tradeRepositoryMock);
+
+            int gameId = 1;
+            int userId = 1;
+
+            AllTrades trades = tradeService.GetAllTradeIdsByGameIdAndUserId(gameId, userId);
+
+            Assert.AreEqual(2, trades.TradeIds.Count());
+        }
+
+        [TestMethod]
+        public void GetTradeByTradeIdTest()
+        {
+            TradeRepositoryMock tradeRepositoryMock = new TradeRepositoryMock();
+            TradeService tradeService = new TradeService(tradeRepositoryMock);
+
+            int tradeId = 1;
+
+            Trade trade = tradeService.GetTradeByTradeId(tradeId);
+
+            Assert.IsNotNull(trade);
+        }
+
+        [TestMethod]
+        public void AcceptTradeTest()
+        {
+            TradeRepositoryMock tradeRepositoryMock = new TradeRepositoryMock();
+            TradeService tradeService = new TradeService(tradeRepositoryMock);
+
+            int tradeId = 1;
+            int userId = 1;
+
+            bool success = tradeService.AcceptTrade(tradeId, userId);
+
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void GetAllTradesByGameId()
+        {
+            TradeRepositoryMock tradeRepositoryMock = new TradeRepositoryMock();
+            TradeService tradeService = new TradeService(tradeRepositoryMock);
+
+            int gameId = 1;
+
+            List<TradeAdmin> trades = tradeService.GetAllTradesByGameId(gameId);
+
+            Assert.AreEqual(1, trades.Count());
+        }
+    }
+}
